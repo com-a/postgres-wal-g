b1487d5719b837fd6935222ce3d89220a4dd4d54
koma <88102376+com-a@users.noreply.github.com>
2023-09-04 21:57:31 +0900

    Update startup.sh

--- data/startup.sh
+++ data/startup.sh
@@ -8,15 +8,13 @@ if [ -s /tmp/bukup_list ]; then
     rm -rf /var/lib/postgresql/data/*
     wal-g backup-fetch /var/lib/postgresql/data LATEST
     #sleep 30
-    #rm /var/lib/postgresql/data/backup_label
-    #rm /var/lib/postgresql//data/backup_label/tablespace_map
     touch "/var/lib/postgresql/data/recovery.signal"
     
 fi
 
 env >> /tmp/crontab
-echo "0 5 * * * wal-g backup-push /var/lib/postgresql/data" >> /tmp/crontab
-echo "0 8 * * *  wal-g delete retain 2 --confirm" >> /tmp/crontab
+echo "0 5 * * * /usr/local/bin/wal-g backup-push /var/lib/postgresql/data" >> /tmp/crontab
+echo "0 8 * * * /usr/local/bin/wal-g delete retain 2 --confirm" >> /tmp/crontab
 crontab /tmp/crontab
 
 /usr/local/bin/docker-entrypoint.sh postgres
