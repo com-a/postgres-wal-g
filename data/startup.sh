2dabdbc416b653d8f74a7e2150cd997434573665
com-a <88102376+com-a@users.noreply.github.com>
2023-11-05 17:24:23 +0900

    a

--- data/startup.sh
+++ data/startup.sh
@@ -12,8 +12,8 @@ if [ -s /tmp/bukup_list_startup ]; then
     
 fi
 
-env >> /tmp/crontab
+#env >> /tmp/crontab
 echo "*/3 * * * * sh /backup.sh" >> /tmp/crontab
 crontab /tmp/crontab
-
+service cron start
 /usr/local/bin/docker-entrypoint.sh postgres
