c4a09a0e9d01c3ae62726d539b4445328b14ef75
koma <88102376+com-a@users.noreply.github.com>
2024-01-05 03:05:15 +0900

    Update setup.sh

--- data/setup.sh
+++ data/setup.sh
@@ -7,6 +7,6 @@ if [ ! -s /tmp/bukup_list_setup ]; then
 
     sed -i -e 's/#archive_mode = off/archive_mode = on/g' /var/lib/postgresql/data/postgresql.conf
     sed -i -e 's/#archive_command = '\'''\''/archive_command = '\''wal-g wal-push %p'\''/g' /var/lib/postgresql/data/postgresql.conf
-    sed -i -e 's/#archive_timeout = 0/archive_timeout = 3600/g' /var/lib/postgresql/data/postgresql.conf
+    sed -i -e 's/#archive_timeout = 0/archive_timeout = 60/g' /var/lib/postgresql/data/postgresql.conf
     sed -i -e 's/#wal_level = replica/wal_level = replica/g' /var/lib/postgresql/data/postgresql.conf
 fi
